445a4d261c3c6134c8ba2692d387b0c4903b5c19
Daniel Koza <dakolech@gmail.com>
2016-03-21 10:49:48 +0100

    configure webpack, install packages

--- index.js
+++ index.js
@@ -3,8 +3,52 @@ import path from 'path';
 
 const app = express();
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
+
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    hot: true,
+    inline: true,
+    stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+    }
+});
+
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.use(express.static('css'));
+app.use(express.static('src'));
+
+app.use(express.static('node_modules/angular'));
+
+app.get('/tags', (req, res) => {
+  res.sendFile(path.join(__dirname, '/tags/all.json'));
+})
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
+  res.render('index');
+});
+
+app.get('/angular', (req, res) => {
+  res.render('src/angular/index');
+});
+
+app.get('/angular/tags-input', (req, res) => {
+  res.render('src/angular/component/tags-input');
 });
 
 app.listen(4000);
