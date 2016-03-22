772e9d118714f039c02d805aff48b6f56be151d1
Daniel Koza <dakolech@gmail.com>
2016-03-22 10:40:50 +0100

    lint fixes

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import app from './app';
 
-import tagsRoutes from './tags/tags.routes'
-import angularRoutes from './angular/angular.routes'
+/* eslint-disable no-unused-vars */
+import tagsRoutes from './tags/tags.routes';
+import angularRoutes from './angular/angular.routes';
+/* eslint-enable no-unused-vars */
 
 app.use(express.static('css'));
 app.use(express.static('src'));
@@ -13,6 +15,3 @@ app.use(express.static('node_modules/bootstrap/dist/css'));
 app.get('/', (req, res) => {
   res.render('index');
 });
-
-
-
