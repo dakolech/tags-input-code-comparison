17a6398a1ee810d0c9c0956153cb66cbc22a682c
Daniel Koza <dakolech@gmail.com>
2016-04-01 09:52:48 +0200

    add new component with template

--- src/vanillajs/app.js
+++ src/vanillajs/app.js
@@ -1,1 +1,3 @@
 import '../shared/shared-libs';
+
+import './component/tags-input.def';
---
+++ src/vanillajs/component/tags-input.def.js
@@ -0,0 +1,7 @@
+import template from './tags-input.jade';
+
+import { Main } from '../lib/main';
+
+Main.newComponent('tags-input', {
+  template: template()
+});
