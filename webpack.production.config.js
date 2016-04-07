81b5a86211f162cfc82ca937e73d4e8afc96771e
Daniel Koza <dakolech@gmail.com>
2016-04-07 11:36:29 +0200

    build fix

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,9 @@
-const webpack = require('webpack');
 const sharedWebpack = require('./webpack.shared');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
   entry: sharedWebpack.entry(),
@@ -8,14 +12,27 @@ module.exports = {
     loaders: sharedWebpack.loaders
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      }
-    }),
-    new webpack.DefinePlugin({
+    new WebpackMd5Hash(),
+    new DedupePlugin(),
+    new DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+
+    new UglifyJsPlugin({
+      beautify: false,
+
+      mangle: {
+        screw_ie8 : true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
+    new CompressionPlugin({
+      regExp: /\.css$|\.html$|\.js$|\.map$/,
+      threshold: 2 * 1024
     })
   ],
   postcss: sharedWebpack.postcss
