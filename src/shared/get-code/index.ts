50720f5b2b3e1e5caf8880fcff50dde4a821d50f
Daniel Koza <dakolech@gmail.com>
2016-04-12 14:49:58 +0200

    typings and code cleanup

--- src/shared/get-code/index.ts
+++ src/shared/get-code/index.ts
@@ -1,7 +1,7 @@
 import { Files } from './github/files.ts';
 import { Listeners } from './lib/listeners.ts';
 
-const codesArray: Array<string> = [
+const codesArray: string[] = [
   'angular',
   'angular-rx',
   'angular2',
@@ -17,17 +17,34 @@ object.files.subscribe((item) => {
 
 const toCompare = [];
 
-class MainIndex {
-  toCompare: Array<string> = [];
+class BetterElement extends Element {
+  public name: string;
+}
 
-  addToCompare(event) {
-    const elementName = event.currentTarget.name;
-    if (!this.toCompare.includes(elementName)) {
-      this.toCompare.push(elementName);
-    }
+class MainIndex {
+  toCompare: string[] = [];
+
+  private switchHidden(elem: Element): void {
+    Array.prototype.forEach.call(elem.querySelectorAll('[switch-hide]'), (item) => {
+      const className = 'hide';
+      item.classList.contains(className) ? item.classList.remove(className) : item.classList.add(className);
+    });
   }
-  
-  compare(event) {
+
+  public compareAction(event: Event): void {
+    const target = <BetterElement>event.currentTarget;
+    const elementName = target.name;
+
+    if (!~this.toCompare.indexOf(elementName)) {
+      this.toCompare.push(elementName);
+    } else {
+      this.toCompare = this.toCompare
+        .filter((item: string) => item !== elementName);
+    }
+    this.switchHidden(target);
+  }
+
+  public compare(event: Event): void {
     console.log(this.toCompare);
   }
 }
--- src/shared/get-code/lib/listeners.ts
+++ src/shared/get-code/lib/listeners.ts
@@ -1,8 +1,6 @@
 export class Listeners {
   static add(element, obj) {
-    console.log(obj)
     Array.prototype.forEach.call(document.querySelectorAll('[on-click]'), (item) => {
-      console.log(obj)
       item.addEventListener('click', (event) => {
         obj[event.currentTarget.getAttribute('on-click').slice(0, -2)](event);
       });
