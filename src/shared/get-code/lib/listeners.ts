d76b274585c6b51d874e40788f8c003892f614a4
Daniel Koza <dakolech@gmail.com>
2016-04-12 16:13:36 +0200

    listeners

--- src/shared/get-code/lib/listeners.ts
+++ src/shared/get-code/lib/listeners.ts
@@ -1,8 +1,13 @@
 export class Listeners {
-  static add(element, obj) {
-    Array.prototype.forEach.call(document.querySelectorAll('[on-click]'), (item) => {
-      item.addEventListener('click', (event) => {
-        obj[event.currentTarget.getAttribute('on-click').slice(0, -2)](event);
+  static add(selector, obj) {
+    document.addEventListener('DOMContentLoaded', () => {
+      const element = document.querySelector(selector);
+      Array.prototype.forEach.call(element.querySelectorAll('[on-click]'), (item) => {
+        item.addEventListener('click', (event) => {
+          if (typeof obj[event.currentTarget.getAttribute('on-click').slice(0, -2)] === 'function') {
+            obj[event.currentTarget.getAttribute('on-click').slice(0, -2)](event);
+          }
+        });
       });
     });
   }
