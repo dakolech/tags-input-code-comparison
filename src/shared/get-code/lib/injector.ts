d8abbc8872b26424b8774910a2c5dc2fdd91566f
Daniel Koza <dakolech@gmail.com>
2016-04-21 15:32:35 +0200

    convert injector class to object

--- src/shared/get-code/lib/injector.ts
+++ src/shared/get-code/lib/injector.ts
@@ -1,13 +1,5 @@
-class InjectorClass {
-  private static instance: InjectorClass;
-  public instances: Object = {};
-
-  static get Instance() {
-      if (this.instance === null || this.instance === undefined) {
-          this.instance = new InjectorClass();
-      }
-      return this.instance;
-  }
+export const Injector = {
+  instances: {},
 
   add<T>(Class: { new(...args: any[]): T }, ...args) {
     const className = new Class().constructor.name;
@@ -15,12 +7,10 @@ class InjectorClass {
     if (!this.instances[className]) {
       this.instances[className] = new Class(...instances);
     }
-  }
+  },
 
   get<T>(Class: { new(...args: any[]): T }): T {
     const className = new Class().constructor.name;
     return this.instances[className];
   }
-}
-
-export const Injector = InjectorClass.Instance;
+};
