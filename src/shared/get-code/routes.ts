c23926f9ed35006fb2803f7727ab17e780ecf8b5
Daniel Koza <dakolech@gmail.com>
2016-04-25 15:27:17 +0200

    create routes

---
+++ src/shared/get-code/routes.ts
@@ -0,0 +1,27 @@
+import { Router } from './lib/router.ts';
+import { Injector } from './lib/injector.ts';
+import { Component } from './lib/components.ts';
+import { PopUp } from './components/popup.ts';
+import { CompareService } from './services/compare.ts';
+import { ShowPopup } from './services/show-popup.ts';
+
+Router.add({
+  name: 'main',
+  path: '/',
+  handler: (params: any) => {
+    Injector.get(ShowPopup).push(false);
+  }
+});
+
+Router.add({
+  name: 'popUp',
+  path: '/compare/:names',
+  handler: (params: any, fromState) => {
+    if (!!params && params.names && fromState.name !== 'main') {
+      Injector.get(CompareService).pushStringArray(params.names);
+    }
+    Component.create('pop-up', new PopUp(Injector.get(CompareService), Injector.get(ShowPopup)));
+  }
+});
+
+Router.listen();
